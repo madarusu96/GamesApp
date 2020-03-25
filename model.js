b978b54bb58eb20a266979da710655ec2927eeaf
madarusu96 <madalinabianca96@gmail.com>
2020-03-25 09:33:42 -0700

    crina

--- model.js
+++ model.js
@@ -1,54 +1,68 @@
-var apiUrl="https://games-world.herokuapp.com"
-fetch(apiUrl +"/games" , {
-    method :"GET",
-    headers : {
-        "Content-Type": "applications/x-www-form-urlencoded"
-    }
+var apiURL = "https://games-world.herokuapp.com";
 
-//pentru a lua doar json si il punem intr-un array jsonResponse
+fetch(apiURL + "/games", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
 }).then(function(response){
     return response.json();
 }).then(function(arrayOfGames){
-    console.log("the response", arrayOfGames)
+    //console.log("the response   ", arrayOfGames);
 
-    var container=document.querySelector('.container');
-    console.log("container")
+    var container1 = document.querySelector('.container');
 
-    // for (i= 0;i<arrayOfGames.length ;i++){
-    //     // console.log(arrayOfGames[i]);
-    //     const h1=document.createElement("h1");
-    //     const p =document.createElement("p");
-    //     const img=document.createElement("img");
+    //console.log(container1);
 
-    //     h1.innerHTML= arrayOfGames[i].title;
-    //     p.innerHTML=arrayOfGames[i].description;
+    // for(var i = 0; i < arrayOfGames.length; i++) {
+    //     //console.log(arrayOfGames[i]);
+    //     const h1 = document.createElement("h1");
+    //     const p = document.createElement("p");
+    //     const img = document.createElement("img");
 
-    //     img.setAttribute("src",arrayOfGames[i].imageUrl);
+    //     h1.innerHTML = arrayOfGames[i].title;
+    //     p.innerHTML = arrayOfGames[i].description;
+
+    //     img.setAttribute("src", arrayOfGames[i].imageUrl);
 
     //     container.appendChild(h1);
     //     container.appendChild(img);
     //     container.appendChild(p);
-
-
-    //     // arrayOfGames[i].title;
-    //     // arrayOfGames[i].description;
-    //     // arrayOfGames[i].imageUrl
     // }
 
-    //optimizare 
-    let gameElements ="";
-    for (i= 0;i<arrayOfGames.length ;i++){
-         gameElements +="<h1>"+arrayOfGames[i].title+"</h1>"+
-                            "<img src='"+ arrayOfGames[i].imageUrl + "'/>" +
-                            "<p>"+arrayOfGames[i].description + "</p>" +
-                            "<button class='delete-btn' id='"+arrayOfGames[i]._id + "'>Delete</button>";
-        
-    }
-    container.innerHTML=gameElements;
+    let gameElements = "";
+    for(var i = 0; i < arrayOfGames.length; i++) {
+        // gameElements += "<h1>" + arrayOfGames[i].title + "</h1>" + 
+        //                     "<img src='" + arrayOfGames[i].imageUrl + "' />" +
+        //                     "<p>" + arrayOfGames[i].description + "</p>" + 
+        //                     "<button class='delete-btn' "+
+        //                         " onclick=\"deleteGame('" + arrayOfGames[i]._id + "')\">Delete</button>";
 
+        gameElements += `<h1>${arrayOfGames[i].title}</h1> 
+                            <img src="${arrayOfGames[i].imageUrl}" />
+                            <p>${arrayOfGames[i].description}</p> 
+                            <button class="delete-btn" onclick="deleteGame('${arrayOfGames[i]._id}')">Delete</button>`;
+
+    }
+    container1.innerHTML = gameElements;
 });
 
-var deleteBtns =document.getElementsByClassName("delete-btn");
-deleteBtns.addEventListener(click,function(){
-    console.log("vom sterge")
-})
+
+function deleteGame(gameID) {
+    //console.log("delete the game ", gameID);
+
+    fetch(apiURL + "/games/" + gameID, {
+        method: "DELETE"
+    }).then(function(r){
+        return r.text();
+    }).then(function(apiresponse){
+        console.log(apiresponse);
+        location.reload(true);
+    });
+
+}
+
+//var deleteBtns = document.getElementsByClassName(".delete-btn")
+
+
+
